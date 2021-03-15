99c4af983ee39134a231ee7b813a314099fcf43b
hamma <mohamedhamma445@gmail.com>
2021-03-15 23:31:14 +0100

    suite

--- src/app/color.directive.ts
+++ src/app/color.directive.ts
@@ -4,32 +4,37 @@ import { Input, OnInit, Directive, ElementRef, HostListener, HostBinding} from '
   selector: '[appColor]'
 })
 export class ColorDirective implements OnInit{
-/*@Input('appColor') public couleurs:{clr:string,bgclr:string};
- @HostListener('mouseenter', ['$event']) onEnter( e: MouseEvent ) {
-console.log(e,this.el);
-console.log('------------'+this.couleurs);
-  this.el.nativeElement.style.color = this.couleurs.clr;
-  this.el.nativeElement.style.backgroundColor = this.couleurs.bgclr;
-  }
-  @HostListener('mouseleave', ['$event']) onLeave( e: MouseEvent ) {
-  console.log(e);
-    this.el.nativeElement.style.color = 'red';
+  @Input('appColor')
+  public couleurs:{clr:string,bgclr:string};
+
+  @HostListener('mouseenter', ['$event'])
+  onEnter( e: MouseEvent ) {
+      console.log(e,this.el);
+      console.log('------------'+this.couleurs);
+      this.el.nativeElement.style.color = this.couleurs.clr;
+      this.el.nativeElement.style.backgroundColor = this.couleurs.bgclr;
     }
-     @HostListener('window:click', ['$event']) onClickWin( e: MouseEvent ) {
+    @HostListener('mouseleave', ['$event'])
+    onLeave( e: MouseEvent ) {
+      console.log(e);
+      this.el.nativeElement.style.color = 'red';
+      this.el.nativeElement.style.backgroundColor = 'white';
+    }
+    @HostListener('window:click', ['$event'])
+    onClickWin( e: MouseEvent ) {
         console.log(e);
-        }
+    }
+    @HostBinding('class')
+    private class:any;
+    @HostBinding('attr.role')
+    private role:any;
 
-  @HostBinding('class')
-  private class:any;
-  @HostBinding('attr.role')
-    private role:any;*/
+    constructor(private el:ElementRef<any>) {
+    }
+    ngOnInit() {
+       //console.log(this.class);
 
-  constructor(private el:ElementRef<any>) {
+       //this.class='test';
+
    }
-  ngOnInit() {
-     //console.log(this.class);
-
-     //this.class='test';
-
-  }
 }
