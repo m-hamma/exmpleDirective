4ccf900a9b30455eac24af64d391af0d2ad5cde5
hamma <mohamedhamma445@gmail.com>
2021-03-15 23:08:02 +0100

    Init directive et route exemple

---
+++ src/app/color.directive.ts
@@ -0,0 +1,35 @@
+import { Input, OnInit, Directive, ElementRef, HostListener, HostBinding} from '@angular/core';
+
+@Directive({
+  selector: '[appColor]'
+})
+export class ColorDirective implements OnInit{
+/*@Input('appColor') public couleurs:{clr:string,bgclr:string};
+ @HostListener('mouseenter', ['$event']) onEnter( e: MouseEvent ) {
+console.log(e,this.el);
+console.log('------------'+this.couleurs);
+  this.el.nativeElement.style.color = this.couleurs.clr;
+  this.el.nativeElement.style.backgroundColor = this.couleurs.bgclr;
+  }
+  @HostListener('mouseleave', ['$event']) onLeave( e: MouseEvent ) {
+  console.log(e);
+    this.el.nativeElement.style.color = 'red';
+    }
+     @HostListener('window:click', ['$event']) onClickWin( e: MouseEvent ) {
+        console.log(e);
+        }
+
+  @HostBinding('class')
+  private class:any;
+  @HostBinding('attr.role')
+    private role:any;*/
+
+  constructor(private el:ElementRef<any>) {
+   }
+  ngOnInit() {
+     //console.log(this.class);
+
+     //this.class='test';
+
+  }
+}
